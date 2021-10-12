91b1bca8367e4c70809ca086dd2ebb0db7c642fd
Rebekah <rebekahmparkes@outlook.com>
2021-10-12 09:17:43 -0400

    init comit

---
+++ two_player_game.rb
@@ -0,0 +1,6 @@
+# Create a 2-Player math game where players take turns to answer simple math addition problems. A new math question is generated for each turn by picking two numbers between 1 and 20. The player whose turn it is is prompted the question and must answer correctly or lose a life.
+
+
+# Both players start with three lives
+# miss q, lose life
+# read player's life amount
